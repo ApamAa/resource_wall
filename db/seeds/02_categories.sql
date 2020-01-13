2621e76971cc049a220b4058da09304eed52140b
ApamAa <apama.jamshidi@gmail.com>
2020-01-13 22:18:36 +0000

    route for categories is made and chendes to the index.ejs file to read from database

--- db/seeds/02_categories.sql
+++ db/seeds/02_categories.sql
@@ -2,8 +2,8 @@ INSERT INTO categories (id, name, category_photo_url) VALUES (1,'TECH','https://
 
 INSERT INTO categories (id, name, category_photo_url) VALUES (2,'ASTRONOMY','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNmhyco1qLDEDsHWrF_uWnrDkJZttLYRWchCel2TQ0e4HXwd6ryA&s' );
 
-INSERT INTO categories (id, name, category_photo_url) VALUES (3,'HISTORY','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcf_2R2tIqw9VucVbj2R5t0PNiBtm-pH4mESLReRikol0aN2Wrzw&s' );
+INSERT INTO categories (id, name, category_photo_url) VALUES (3,'HISTORY','https://cropper.watch.aetnd.com/cdn.watch.aetnd.com/sites/2/2015/09/ancient-aliens-S12-desktop-2048x1152.jpg?w=1440');
 
 INSERT INTO categories (id, name, category_photo_url) VALUES (4,'SCIENCE','https://iiif.wellcomecollection.org/image/V0025489.jpg/0,0,2048,2048/256,/0/default.jpg' );
 
-INSERT INTO categories (id, name, category_photo_url) VALUES (5,'BIOLOGY','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbS0kwD50eZjx7OkGC_CJgDR3gxUwiF_nsYL7owI251CGjCUmnVw&s' );
+INSERT INTO categories (id, name, category_photo_url) VALUES (5,'BIOLOGY','https://images.pexels.com/photos/920157/pexels-photo-920157.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' );
